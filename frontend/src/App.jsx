84dbdcec4c1daa557e0627b2e294552e2a176f74
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-03-18 19:08:46 -0500

    Creo DetalleProducto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/NavBar';
 import Home from './pages/Home';
+import DetalleProducto from './pages/DetalleProducto';
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="*" element={<h1>Página no encontrada</h1>} />
+                    <Route path="producto/:id" element={<DetalleProducto/>} />
                 </Routes>
             </div>
         </Router>
---
+++ frontend/src/pages/DetalleProducto.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from "react-router-dom";
+import { fetchData } from '../api';
+
+const DetalleProducto = () => {
+    const { id } = useParams();
+    const [producto, setProducto] = useState({});
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchProducto = async () => {
+            try {
+                const response = await fetchData (api/productos/{ id });
+                const data = await response.json();
+                if (data) {
+                    setProducto(data);
+                } else {
+                    console.log("No existe el producto");
+                }
+            } catch {
+                setError("Error al cargar el producto");
+
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProducto();
+    }, [id]);
+    return(
+        <>
+        <h1>{nombre}</h1>
+        </>
+    )
+}
+
+export default DetalleProducto;
