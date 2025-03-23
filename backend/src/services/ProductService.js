be0e6b4690e34e2c70a3d26d65af2d4529d22eed
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-03-23 13:04:23 -0500

    Corrijo nombre getProductById(Mal escrito) en ProductService

--- backend/src/services/ProductService.js
+++ backend/src/services/ProductService.js
@@ -17,14 +17,14 @@ const ProductService = {
         }
     },
 
-    async getProductByID(id) {
+    async getProductById(id) {
         try {
             const result = await pool.query("SELECT id, nombre, descripcion, precio, fotos FROM productos WHERE id=$1", [id]);
-            if (rows.lenght === 0) {
+            if (result.rows.length === 0) {
                 return console.log("Producto no encontrado");
             }
             return result.rows[0];
-        } catch {
+        } catch (error) {
             throw error;
         }
     }
