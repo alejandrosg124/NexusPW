18e4f3c2d0696c7c8d400a36f41c687580af21de
Alejandro <48536321+alejandrosg124@users.noreply.github.com>
2025-03-23 12:53:27 -0500

    Actualizo controller

--- backend/src/controllers/ProductController.js
+++ backend/src/controllers/ProductController.js
@@ -2,25 +2,26 @@ import ProductService from '../services/ProductService.js';
 
 const ProductController = {
     async getAllProducts(req, res) {
-        try{
+        try {
             const productos = await ProductService.getAllProducts();
             res.json(productos);
         } catch (error) {
-            console.log("Error al obtener productos")
+            console.log("Error al obtener productos", error);
+            res.status(500).json({ error: "Error al obtener productos" });
         }
     },
 
-    async getProductById(req,res) {
+    async getProductById(req, res) {
         try {
-        const { id } = req.params;
-        const producto = await ProductService.getProductById(id);
-        if (!producto) {
-            return res.status(404).json({ error: "Producto no encontrado" });
-        }
-        res.json(producto);
-        } catch {
-            console.log("Error al obtener producto")
-            req.status(500).json({error: "Error al obtener producto"});
+            const { id } = req.params;
+            const producto = await ProductService.getProductById(id);
+            if (!producto) {
+                return res.status(404).json({ error: "Producto no encontrado" });
+            }
+            res.json(producto);
+        } catch (error) {
+            console.log("Error al obtener producto", error);
+            res.status(500).json({ error: "Error al obtener producto" });
         }
     }
 }
